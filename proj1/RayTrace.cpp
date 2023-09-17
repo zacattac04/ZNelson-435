a2cbcc8dbb83bc4c6e4eade7f07600b2be270cdc
zacattac04 <zacharyjn@gmail.com>
2023-09-17 17:01:46 -0400

    First working copy
    Able to read all of the data from the nff file

--- proj1/RayTrace.cpp
+++ proj1/RayTrace.cpp
@@ -1,13 +1,15 @@
 #include "RayTrace.h"
+#include <sstream>
+#include <fstream>
 
 
 Tracer::Tracer(const string &fname) {
-    ifstream istream(fname.c_str(), std::ios_base);
+    ifstream istream(fname.c_str() ,ios_base::in);
     string line;
     char ch;
-
-    while (istream.is_open()){
-        getLine(istream, line);
+    int i = 0;
+    while (!istream.eof()){
+        getline(istream, line);
         switch (line[0]) {
             case 'b': {
                 stringstream ss(line);
@@ -15,40 +17,36 @@ Tracer::Tracer(const string &fname) {
                 break;
             }
             case 'v': {
-                getLine(istream, line);
+                getline(istream, line);
                 string junk;
                 stringstream fromss(line);
                 fromss>>junk>>eye[0]>>eye[1]>>eye[2];
                 
-                getLine(istream, line);
+                getline(istream, line);
                 stringstream atss(line);
                 atss>>junk>>at[0]>>at[1]>>at[2];
 
-                getLine(istream, line);
+                getline(istream, line);
                 stringstream upss(line);
                 upss>>junk>>up[0]>>up[1]>>up[2];
 
-                getLine(istream, line);
+                getline(istream, line);
                 stringstream angless(line);
                 angless>>junk>>angle;
 
-                getLine(istream, line);
+                getline(istream, line);
                 stringstream hitherss(line);
                 hitherss>>junk>>hither;
 
-                getLine(istream,line);
+                getline(istream,line);
                 stringstream resss(line);
                 resss>>junk>>res[0]>>res[1];
                 break;
             }
 
-            case 'l': {
-                break;
-            }
-
             case 'f': {
                 stringstream ss(line);
-                ss>>fill.color[0]>>fill.color[1]>>fill.color[2]>>fill.kd>>fill.ks>>fill.shine>>fill.t>>fill.ior;
+                ss>>ch>>fill.color[0]>>fill.color[1]>>fill.color[2]>>fill.kd>>fill.ks>>fill.shine>>fill.t>>fill.ior;
                 break;
             }
             case 'p': {
@@ -56,32 +54,38 @@ Tracer::Tracer(const string &fname) {
                 int numCoords;
                 ss>>ch>>numCoords;
                 if (numCoords == 3) {
-                    Triangle triangle;
-
-                    getLine(iStream, line);
+                    getline(istream, line);
                     stringstream ass(line);
-                    ass>>triangle.a[0]>>triangle.a[1]>>triangle.a[2];
+                    Eigen::Vector3d A;
+                    ass>>A[0]>>A[1]>>A[2];
 
-                    getLine(iStream, line);
+                    getline(istream, line);
                     stringstream bss(line);
-                    bss>>triangle.b[0]>>triangle.b[1]>>triangle.b[2];
+                    Eigen::Vector3d B;
+                    bss>>B[0]>>B[1]>>B[2];
 
-                    getLine(iStream, line);
-                    stringStream css(line);
-                    css>>triangle.c[0]>>triangle.c[1]>>triangle.c[2];
-
-                    surfaces.insert(triangle);
+                    getline(istream, line);
+                    stringstream css(line);
+                    Eigen::Vector3d C;
+                    css>>C[0]>>C[1]>>C[2];
+                    //Triangle* triangle = new Triangle(A, B, C);
+                    surfaces.push_back(new Triangle(A, B, C));
                 }
+                break;
+            }
+            default: {
+                break;
             }
         }
     }
+    istream.close();
 }
 
-Tracer::details(){
+void Tracer::details(){
     cout << "Background Color:" << endl;
     cout << "R: " <<bcolor[0] << "\tB: " << bcolor[1] << "\tG: " << bcolor[2] << endl << endl;
     cout << "Viewpoint details:" << endl;
-    cout << "Eye: " << eye[0] << "\t" << eye[1] << "\t" << eye[2] << endl:
+    cout << "Eye: " << eye[0] << "\t" << eye[1] << "\t" << eye[2] << endl;
     cout << "At: " << at[0] << "\t" << at[1] << "\t" << at[2] << endl;
     cout << "Up: " << up[0] << "\t" << up[1] << "\t" << up[2] << endl;
     cout << "Angle: " << angle << endl;
@@ -92,7 +96,7 @@ Tracer::details(){
     cout << "Surfaces:" << endl;
     
     for (int i = 0; i < surfaces.size(); i++) {
-
+        surfaces[i]->details();
     }
 
 }
@@ -103,15 +107,23 @@ double det(const Eigen::Vector3d &a, const Eigen::Vector3d &b, const Eigen::Vect
             c[0] * (a[1] * b[2] - b[1] * a[2]);
 }
 
-Triangle::details() {
-    cout << "Type: Triangle";
+void Triangle::details() {
+    cout << "Type: Triangle" << endl;
     cout << a[0] << "\t" << a[1] << "\t" << a[2] << endl;
     cout << b[0] << "\t" << b[1] << "\t" << b[2] << endl;
     cout << c[0] << "\t" << c[1] << "\t" << c[2] << endl;
 }
 
+Triangle::Triangle(Eigen::Vector3d& A, Eigen::Vector3d& B, Eigen::Vector3d& C) {
+    a = A;
+    b = B;
+    c = C;
+    type = "Triangle";
+
+}
 int main(int argc, const char * argv[]) {
-    Tracer tracer(argv[optind++]);
+    Tracer tracer(argv[1]);
+    cout << "printing details:" << endl;
     tracer.details();
     return 0;
 }
--- proj1/RayTrace.h
+++ proj1/RayTrace.h
@@ -1,6 +1,7 @@
 #ifndef RAYTRACE_H
 #define RAYTRACE_H
 #include <iostream>
+#include <stdio.h>
 #include <string>
 #include <Eigen/Dense>
 
@@ -8,13 +9,57 @@ using namespace std;
 
 
 
+
+class Surface {
+public:
+    Surface() {
+        type = "null";
+    }
+    virtual void details() = 0;
+    //HitRecord hit(const Ray &r, double t0, double t1) const;
+protected:
+    string type;
+
+};
+
+class Triangle: public Surface{
+public:
+    Triangle();
+    Triangle(Eigen::Vector3d& A, Eigen::Vector3d& B, Eigen::Vector3d& C);
+    void details();
+private:
+    Eigen::Vector3d a;
+    Eigen::Vector3d b;
+    Eigen::Vector3d c;
+};
+
+/*
+class HitRecord {
+public:
+    HitRecord();
+private:
+    Surface s;
+    double t;
+    Eigen::Vector3d n;
+
+};
+*/
+struct Fill {
+    Eigen::Vector3d color;
+    double kd;
+    double ks;
+    double shine;
+    double t;
+    double ior;
+};
+
 class Tracer {
 public:
     Tracer(const string &fname);
-    Eigen::Vector3d shade(const HitRecord &hr) const;
-    Eigen::Vector3d castRay(const Ray &r, double t0, double t1) const;
-    void createImage();
-    void writeImage(const string &fname);
+    //Eigen::Vector3d shade(const HitRecord &hr) const;
+    //Eigen::Vector3d castRay(const Ray &r, double t0, double t1) const;
+    //void createImage();
+    //void writeImage(const string &fname);
     void details();
 
 private:
@@ -26,47 +71,7 @@ private:
     Eigen::Vector2d res;
     Eigen::Vector3d bcolor;
     Fill fill;
-    vector<Surface> surfaces;
-}
+    vector<Surface*> surfaces;
+};
 
-class Surface {
-public:
-    Surface();
-    void details();
-    HitRecord hit(const Ray &r, double t0, double t1) const;
-protected:
-
-
-}
-
-class Triangle: public Surface{
-    Triangle(Eigen::Vector3d &a, Eigen::Vector3d &b, Eigen::Vector3d &c);
-private:
-    Eigen::Vector3d a;
-    Eigen::Vector3d b;
-    Eigen::Vector3d c;
-}
-
-class HitRecord {
-public:
-    HitRecord();
-private:
-    Surface s;
-    double t;
-    Eigen::Vector3d n;
-
-}
-
-class Fill {
-public:
-    Fill();
-private:
-    Eigen::Vector3d color;
-    double kd;
-    double ks;
-    double shine;
-    double t;
-    double ior;
-
-
-}
+#endif
